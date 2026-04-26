8646a977624041bcfb7a4ac9e909a749b1228203
chandan-cpu <chandanbo345@gmail.com>
2026-04-26 13:16:33 +0530

    correction

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const uri= process.env.MONGO_URI || 'mongodb://localhost:27017/api-gateway';
+        const uri= process.env.MONGO_URI;
         // .env file se URI utha kar connect karega
         const conn = await mongoose.connect(uri);
         console.log(`🚀 MongoDB Connected Successfully: ${conn.connection.host}`);
